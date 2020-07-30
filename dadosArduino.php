cbe5aa76cc2c50a89e0d32d77fba96a21825dd26
AndersonGuedes07 <andersonpgca@hotmail.com>
2020-07-30 17:12:40 -0300

    Testando a conexao do arduino com Banco de Dados
    Conectando o arduino com o site e captando os dados dos sensores e enviando para o o banco de dados

---
+++ dadosArduino.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    $s1_recebido = $_GET['s1'];
+    $s2_recebido = $_GET['s2'];
+    $s3_recebido = $_GET['s3'];
+    
+    echo "Sensor 1 Recebido: ". $sensor1_recebido;
+    echo "<br/>";
+    echo "Sensor 2 Recebido: ". $sensor2_recebido;
+    echo "<br/>";
+    echo "Sensor 3 Recebido: ". $sensor3_recebido;
+*/
+?>
---
+++ salvar.php
@@ -0,0 +1,22 @@
+<?php
+    include "conexao.php";
+
+    $s1_rec = $_GET['s1'];
+    $s2_rec = $_GET['s2'];
+    $s3_rec = $_GET['s3'];
+
+    $SQL_INSERT = "INSERT INTO tbsensores (sensor1, sensor2, sensor3) VALUES (:S1, :S2, :S3)";
+
+    $stmt = $conexao->prepare($SQL_INSERT);
+
+    $stmt->bindParam(":S1", $s1_rec);
+    $stmt->bindParam(":S2", $s2_rec);
+    $stmt->bindParam(":S3", $s3_rec);
+
+    if($stmt->execute()){
+        echo "insert ok";
+    } else {
+        echo "insert_erro";
+    }
+
+?>
