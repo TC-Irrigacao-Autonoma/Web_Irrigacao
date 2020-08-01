da8c7299f92b67586aa15e0431accdfc86007d4d
AndersonGuedes07 <andersonpgca@hotmail.com>
2020-08-01 18:57:24 -0300

    Update conexao.php

--- Arduino/conexao.php
+++ Arduino/conexao.php
@@ -1,11 +1,11 @@
 <?php
 
-    $CON_CONEXAO = "mysql:host=200.170.81.48;dbname=gaviaope_agroirrigadb;charset=utf8";
+    $CON_CONEXAO = "mysql:host=cp01-do-nyc3;dbname=gaviaope_agroirrigadb;charset=utf8";
     $CON_USUARIO = "gaviaope_agroirriga";
     $CON_SENHA = "405937irriga";
 
     try{
-
+    
         $conexao = new PDO("$CON_CONEXAO", "$CON_USUARIO", "$CON_SENHA");
         echo "Conectado com sucesso";
 
