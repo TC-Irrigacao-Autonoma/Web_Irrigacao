92e7360fc0fe2d6e1aee667e401e6c37c1a076cf
AndersonGuedes07 <andersonpgca@hotmail.com>
2020-08-05 17:58:14 -0300

    deletou o comentario da conexao maq. local

--- Arduino/conexao.php
+++ Arduino/conexao.php
@@ -1,17 +1,10 @@
 <?php
 
-    
     // ******* BANCO DE DADOS HOSPEDAGEM AGROIRRIGA ****** 
     $CON_CONEXAO = "mysql:host=localhost;dbname=gaviaope_agroirrigadb;charset=utf8";
     $CON_USUARIO = "gaviaope_agroirriga";
     $CON_SENHA = "405937irriga";
-    
 
-    /*
-    $CON_CONEXAO = "mysql:host=localhost;dbname=bdarduino;charset=utf8";
-    $CON_USUARIO = "root";
-    $CON_SENHA = "";
-    */
     try{
     
         $conexao = new PDO("$CON_CONEXAO", "$CON_USUARIO", "$CON_SENHA");
